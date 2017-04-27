cfd341c7bca9d2374107d21644de0ec345ebe8e1
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-04-27 14:56:12 -0400

    The rect-ing works

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ import pygame
 # Import the player class from player
 from random import randint
 from player import Player
+from enemy import Enemy
 from game_functions import check_events
+from pygame.sprite import Group, groupcollide
 
 # THE CORE GAME FUNCTIONALITY/LOOP
 def run_game():
@@ -15,33 +17,52 @@ def run_game():
   # Set up a tuple for the screen size: (horizontal, vertical)
   screen_size = (1000,800)
   # Set up a tuple for the bg color
-  background_color = (140,200,150)
+  # background_color = (140,200,150)
+  background_color = (255,245,255)
 
   # Create a pygame screen to use
   screen = pygame.display.set_mode(screen_size)
   # Set a caption on the terminal window
-  pygame.display.set_caption("A heroic 3rd person shooter")
+  pygame.display.set_caption("A the_player.ic 3rd person shooter")
 
-  the_player = Player(screen)
+  the_player = Player(screen,"./images 2/Hero.png",200,200)
+  the_enemy = Enemy(screen)
+  the_player_group = Group()
+  the_player_group.add(the_player)
+  enemies = Group()
+  enemies.add(the_enemy)
 
-  r = 0
-  b = 0
-  g = 0
+  # r = 0
+  # b = 0
+  # g = 0
 
   # Main game loop run forever or until break
   while 1:
-    # if r >=
-    # r += 10
-    # b += 10
-    # g += 10
+    # r += 1
+    # b += 2
+    # g += 3
+    # if r == 255:
+    #   r = 130
+    # if b >= 250:
+    #   b = 30
+    # if g >= 250:
+    #   g = 0
     # background_color = (r,b,g)
     screen.fill(background_color)
     # The escape hatch from while
 
-    check_events()
+    check_events(the_player)
 
     # Draw the player
-    the_player.draw_me()
+    for player in the_player_group:
+      the_player.draw_me()
+    
+    the_enemy.update_me(the_player)
+    
+    for enemy in enemies:
+      the_enemy.draw_me()
+
+    player_died = groupcollide(the_player_group, enemies, False, True)
 
         # Clear the screen for the next time through the loop
     pygame.display.flip()
--- game_functions.py
+++ game_functions.py
@@ -3,8 +3,30 @@ import pygame
 # We need sys for quit
 import sys
 
-def check_events():
+def check_events(the_player):
   for event in pygame.event.get():
+
     if event.type == pygame.QUIT:
     # The user clicked the red x. Get out of the loop and kill the game
       sys.exit()
+
+    elif event.type == pygame.KEYDOWN:
+      if event.key == 273:
+        the_player.should_move("up", True)
+      elif event.key == 274: 
+        the_player.should_move("down", True)
+      if event.key == 275:
+        the_player.should_move("right", True)
+      elif event.key == 276:
+        the_player.should_move("left", True)
+      # print event.key
+
+    elif event.type == pygame.KEYUP:
+      if event.key == 273:
+        the_player.should_move("up", False)
+      elif event.key == 274:
+        the_player.should_move("down", False)
+      if event.key == 275:
+        the_player.should_move("right", False)
+      elif event.key == 276:
+        the_player.should_move("left", False)
